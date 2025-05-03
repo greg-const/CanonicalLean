1460c1eedda1cf066f0a1b3ebcaee8f66c1af900
Chase Norman <c_@berkeley.edu>
2025-05-03 15:31:48 -0400

    buildArchive

--- lakefile.lean
+++ lakefile.lean
@@ -1,8 +1,19 @@
 import Lake
 open Lake DSL
 
+/-- Mirrors the releases of Lean -/
+def buildArchive :=
+  if System.Platform.isWindows then "windows"
+  else if System.Platform.isOSX then
+    if System.Platform.target.startsWith "x86_64" then "darwin"
+    else "darwin_aarch64"
+  else if System.Platform.numBits == 32 then "linux"
+  else if System.Platform.target.startsWith "aarch64" then "linux_aarch64"
+  else "linux_x86"
+
 package Canonical where
   preferReleaseBuild := true
+  buildArchive := buildArchive
 
 target canonical pkg : Dynlib := do pure $ Job.pure {
   path := pkg.sharedLibDir / nameToSharedLib "canonical_lean"
