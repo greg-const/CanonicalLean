7e161a6f3d0fb8ec04a9984706d49d24cb3fb49c
Chase Norman <c_@berkeley.edu>
2025-05-03 10:26:10 -0400

    change dynlib names

--- lakefile.lean
+++ lakefile.lean
@@ -4,8 +4,10 @@ open Lake DSL
 package Canonical where
   precompileModules := true
 
-target canonical : Dynlib := do pure (Job.pure
-  {path := __dir__ / ".lake" / "build" / "lib" / nameToSharedLib "canonical", name := "canonical"})
+target canonical : Dynlib := do pure $ Job.pure {
+  path := __dir__ / ".lake" / "build" / "lib" / nameToSharedLib "canonical_lean"
+  name := "canonical_lean"
+}
 
 @[default_target]
 lean_lib Canonical {moreLinkLibs := #[canonical]}
