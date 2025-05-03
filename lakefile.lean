f696a3cb0bd5fcd030939b618a8b0b4da03043fb
Chase Norman <c_@berkeley.edu>
2025-05-03 13:19:21 -0400

    more lakefile updates

--- lakefile.lean
+++ lakefile.lean
@@ -3,13 +3,12 @@ open Lake DSL
 
 package Canonical
 
-target canonical : Dynlib := do pure $ Job.pure {
-  path := __dir__ / ".lake" / "build" / "lib" / nameToSharedLib "canonical_lean"
+target canonical pkg : Dynlib := do pure $ Job.pure {
+  path := pkg.sharedLibDir / nameToSharedLib "canonical_lean"
   name := "canonical_lean"
 }
 
 @[default_target]
-lean_lib Canonical {
+lean_lib Canonical where
   precompileModules := true
   moreLinkLibs := #[canonical]
-}
